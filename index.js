303b454ef681feb70baa96658ee9b8a853b30ea2
ddderi <maxime830@live.fr>
2021-12-13 07:28:11 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2; 
+let num3 = 20;
+let num4 = 5;
+
+var multiply = num1 * num2;
+
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+// console.log(random);
+
+var mod = num3 / num4
+
+var max = Math.max(1, 2, 3, 20);
